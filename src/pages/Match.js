3f9ddfcd4ade1d57e4056c5fffd22dd8b60cd919
element3988 <richardm3988@gmail.com>
2020-04-16 12:44:21 -0400

    Finished

--- src/pages/Match.js
+++ src/pages/Match.js
@@ -10,16 +10,16 @@ const Match = (props) => {
   return (
     <div className='container' id='matchBox'>
       Hello.
-      {/* <h1 id='matched'>Matched</h1>
+      <h1 id='matched'>Matched</h1>
       <iframe width="100%" height="100%" frameborder="0" style={{border:'0'}}
-        src={`https://www.google.com/maps/embed/v1/place?q=${props.name}%2C%20${props.zipcode}&key=${GOOGLE}`} allowfullscreen></iframe>
+        src={`https://www.google.com/maps/embed/v1/place?q=${props.yesbin}%2C%20${props.zipcode}&key=${GOOGLE}`} allowfullscreen></iframe>
       <Button className='btn btn-dark' style={{background: 'transparent'}}>
         <Link to='/start' style={{
           color: '#fff', fontFamily: "Rochester", padding: '1rem', width: '300', height: 'auto'
         }}>
           Go Again?
         </Link>
-      </Button> */}
+      </Button>
     </div>
   );
 };
